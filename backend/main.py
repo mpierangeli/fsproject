e42b11494904c0e7ec0fdeb4d89666cfaeab43b2
mpierangeli <mpierangeli@estudiantes.unsam.edu.ar>
2024-03-28 18:46:09 -0300

    Added login cookie + db tables + platform design

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, Request, Form
+from fastapi import FastAPI, Depends, HTTPException, Request, Form, Cookie
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -7,6 +7,15 @@ import crud
 from database import engine, localSession
 from schemas import UserData, UserId
 from models import Base
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+from typing import Annotated
+
+load_dotenv()
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+TOKEN_EXP_SECS = os.getenv('TOKEN_EXP_SECS')
 
 Base.metadata.create_all(bind=engine) # Create the tables in the database if not exist
 
@@ -20,6 +29,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def create_token(data:dict = {}):
+    token_data = data.copy()
+    token_data["exp"] = datetime.utcnow() + timedelta(seconds=int(TOKEN_EXP_SECS))
+    token_jwt = jwt.encode(token_data, JWT_SECRET_KEY, algorithm="HS256")
+    return token_jwt
 
 @app.get("/", response_class=HTMLResponse)
 def root_get(request: Request):
@@ -41,15 +56,49 @@ def create_user(username: str = Form(...), email: str = Form(...), password: str
     return RedirectResponse(url="/")
 
 @app.post("/users/login")
-def login(request: Request, username: str = Form(...), password:str = Form(...), db: Session = Depends(get_db)):
+def login(username: str = Form(...), password:str = Form(...), db: Session = Depends(get_db)):
     db_user = crud.validate_user(db, username, password)
     if db_user is None:
         return "Username or Password Incorrect"
-    return templates.TemplateResponse("platform.html", {"request": request, "user":db_user})
+    token = create_token({"user": db_user.username})
+    return RedirectResponse(url="/users/platform",
+                            status_code=302, 
+                            headers={"set-cookie": f'access_token={token}; Max-Age={TOKEN_EXP_SECS}; Path=/'})
+
+@app.get("/users/logout")
+def logout():
+    return RedirectResponse(url="/", status_code=302, headers={"set-cookie": "access_token=; Max-Age=0; Path=/"})
+
+@app.get("/users/platform")
+def platform(request: Request, access_token: Annotated[str | None, Cookie()] = None, db: Session = Depends(get_db)):
+    if access_token is None:
+        return RedirectResponse(url="/")
+    try:
+        user_data = jwt.decode(access_token, JWT_SECRET_KEY, algorithms=["HS256"])
+        if crud.get_user_by_username(db, user_data["user"]) is None:
+            return RedirectResponse(url="/")
+        return templates.TemplateResponse("platform.html", {"request": request, 
+                                                            "user":user_data["user"], 
+                                                            "publications":[{"title":"Title 1", "content":"Content 1", "author":"Author 1", "date":"2021-10-01"},
+                                                                            {"title":"Title 2", "content":"Content 2", "author":"Author 2", "date":"2021-10-02"},
+                                                                            {"title":"Title 3", "content":"Content 3", "author":"Author 3", "date":"2021-10-03"}
+                                                            ]
+                                                            })
+    except JWTError:
+        return RedirectResponse(url="/")
 
 @app.get("/toggleSignup")
 def toggleSignup(request: Request):
     return templates.TemplateResponse("signup-form.html", {"request": request})
 @app.get("/toggleLogin")
-def toggleSignup(request: Request):
-    return templates.TemplateResponse("login-form.html", {"request": request})
+def toggleSignup(request: Request, access_token: Annotated[str | None, Cookie()] = None, db: Session = Depends(get_db)):
+    if access_token is None:
+        return templates.TemplateResponse("login-form.html", {"request": request})
+    try:
+        user_data = jwt.decode(access_token, JWT_SECRET_KEY, algorithms=["HS256"])
+        if crud.get_user_by_username(db, user_data["user"]) is None:
+            return RedirectResponse(url="/")
+        return templates.TemplateResponse("platform.html", {"request": request, "user":user_data["user"], "token":access_token})
+    except JWTError:
+        return templates.TemplateResponse("login-form.html", {"request": request})
+    
--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,23 @@ class User(Base):
     email = Column(String(30), unique=True, nullable=False)
     salt = Column(LargeBinary, nullable=False)
     password = Column(String(64), nullable=False)
+
+class Publication(Base):
+    __tablename__ = 'publications'
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    title = Column(String(100), nullable=False)
+    content = Column(String(500), nullable=False)
+    author = Column(Integer, nullable=False)
+    date = Column(String(10), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    content = Column(String(500), nullable=False)
+    author = Column(Integer, nullable=False)
+    publication = Column(Integer, nullable=False)
+    date = Column(String(10), nullable=False)
+
+    
